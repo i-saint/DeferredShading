1d60954b92aba18c60f70ad1958c28dbed289bb6
i-saint <saint.skr@gmail.com>
2014-12-15 21:50:23 +0900

    adding beam (wip)

--- Assets/DeferredShading/Scripts/DSBeam.cs
+++ Assets/DeferredShading/Scripts/DSBeam.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class DSBeam : MonoBehaviour
 {
+    public enum State
+    {
+        Active,
+        Fading,
+    }
+
     public float speed = 20.0f;
     public float length = 0.0f;
+    public float fade_speed = 0.025f;
+    public float lifetime = 2.0f;
+    public float time = 0.0f;
+    public State state = State.Active;
     Transform trans;
     MeshRenderer mesh_renderer;
     MaterialPropertyBlock property_block;
@@ -14,20 +24,43 @@ public class DSBeam : MonoBehaviour
     {
         trans = GetComponent<Transform>();
         property_block = new MaterialPropertyBlock();
-        property_block.AddMatrix("prev_Object2World", Matrix4x4.identity);
+        property_block.AddVector("beam_direction", beam_params);
+        property_block.AddVector("base_position", trans.position);
         mesh_renderer = GetComponent<MeshRenderer>();
         mesh_renderer.SetPropertyBlock(property_block);
     }
 
     void Update()
     {
-        length += speed * Time.deltaTime;
+        time += Time.deltaTime;
+        length = speed * time;
         beam_params.Set(trans.forward.x, trans.forward.y, trans.forward.z, length);
+
+        if (state==State.Active)
+        {
+            if (time > lifetime)
+            {
+                state = State.Fading;
+            }
+        }
+        else if (state == State.Fading)
+        {
+            Vector3 scale = trans.localScale;
+            scale.x -= fade_speed;
+            scale.y -= fade_speed;
+            scale.z -= fade_speed;
+            trans.localScale = scale;
+            if (scale.x <= 0.0f)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     void OnWillRenderObject()
     {
         property_block.SetVector("beam_direction", beam_params);
+        property_block.SetVector("base_position", trans.position);
         mesh_renderer.SetPropertyBlock(property_block);
     }
 }
---
+++ Assets/Scripts/TestBeam.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+
+public class TestBeam : MonoBehaviour
+{
+    public DSRenderer cam;
+    public bool showGUI;
+    public bool rotateByTime = true;
+    public GameObject beam_prefab;
+
+
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        CameraControl();
+
+        if (Time.frameCount % 30==0)
+        {
+            Instantiate(beam_prefab,
+                new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-1.0f, 2.0f)+1.0f, Random.Range(-4.0f, 4.0f)-10.0f),
+                Quaternion.Euler(0.0f, 0.0f, 0.0f));
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            showGUI = !showGUI;
+        }
+    }
+
+    void CameraControl()
+    {
+        //Vector3 pos = Quaternion.Euler(0.0f, Time.deltaTime * -15.0f, 0) * cam.transform.position;
+        Vector3 pos = cam.transform.position;
+        if (Input.GetKeyUp(KeyCode.R)) { rotateByTime = !rotateByTime; }
+        if (rotateByTime)
+        {
+            pos = Quaternion.Euler(0.0f, Time.deltaTime * -10.0f, 0) * pos;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            float ry = Input.GetAxis("Mouse X") * 3.0f;
+            float rxz = Input.GetAxis("Mouse Y") * 0.25f;
+            pos = Quaternion.Euler(0.0f, ry, 0) * pos;
+            pos.y += rxz;
+        }
+        {
+            float wheel = Input.GetAxis("Mouse ScrollWheel");
+            pos += pos.normalized * wheel * 4.0f;
+        }
+        cam.transform.position = pos;
+        cam.transform.LookAt(new Vector3(0.0f, 2.0f, 0.0f));
+    }
+
+    void OnGUI()
+    {
+        float lineheight = 22.0f;
+        float margin = 0.0f;
+        float labelWidth = 130.0f;
+        float x = 10.0f;
+        float y = 10.0f;
+
+        if (!showGUI) { return; }
+
+
+        GUI.Label(new Rect(x, y, 300, lineheight), "mouse drag & wheel: move camera");
+        y += lineheight + margin;
+
+        GUI.Label(new Rect(x, y, 300, lineheight), "space: show / hide GUI");
+        y += lineheight + margin;
+        GUI.Label(new Rect(x, y, 300, lineheight), "R: camera rotation on / off");
+        y += lineheight + margin;
+    }
+}
