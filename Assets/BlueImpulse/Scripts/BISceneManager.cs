22dca6fbcab2e7492e14710f808fe46737847f57
i-saint <saint.skr@gmail.com>
2015-02-14 07:54:38 +0900

    Blue Impulse

---
+++ Assets/BlueImpulse/Scripts/BISceneManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BISceneManager : MonoBehaviour
+{
+    public bool m_show_time = false;
+    public float m_time_scale = 1.0f;
+    bool m_dirty;
+
+    void Update()
+    {
+        if(m_dirty)
+        {
+            Time.timeScale = m_time_scale;
+        }
+    }
+
+#if UNITY_EDITOR
+    void OnGUI()
+    {
+        if (m_show_time)
+        {
+            GUI.Label(new Rect(5,5,200,20), "time: "+Time.time);
+        }
+    }
+
+    void OnValidate()
+    {
+        m_dirty = true;
+    }
+#endif // UNITY_EDITOR
+}
---
+++ Assets/BlueImpulse/Scripts/UIFadeParams.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIFadeParams : MonoBehaviour
+{
+    public Image m_fader;
+    public Text m_code;
+    public Text m_sound;
+    public RawImage m_unity_logo;
+
+    public float m_fader_alpha;
+    public float m_code_alpha;
+    public float m_sound_alpha;
+    public float m_logo_alpha;
+
+    void Update ()
+    {
+        m_fader.color = new Color(0, 0, 0, m_fader_alpha);
+        m_code.color = new Color(1, 1, 1, m_code_alpha);
+        m_sound.color = new Color(1, 1, 1, m_sound_alpha);
+        m_unity_logo.color = new Color(1, 1, 1, m_logo_alpha);
+    }
+}
